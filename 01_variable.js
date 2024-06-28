9713dd363e5c6dd850184a7833fe3f48c3fb8c4b
Gagandeep singh <gaganrobin71@gmail.com>
2024-06-28 10:37:52 +0530

    add variable file

---
+++ 01_variable.js
@@ -0,0 +1,19 @@
+const  accountId = 144553 //never change
+let accountEmail ="gaganrobin71gmail.com"  //
+var accountPassword ="12345" //
+accountCity ="Jaipur"//not allow but can do
+let accountState;
+// accountId = 2 // not allow
+
+accountEmail ="hjgcshG@gmail.com"
+accountPassword ="34635634"
+accountCity ="amritar"
+
+console.log(accountId);
+
+
+/*
+prefer not use to var 
+because  of issue in block scope and functional scope
+*/
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState])
