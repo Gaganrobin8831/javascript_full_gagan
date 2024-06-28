2212502f153bfb411209b72d453de55794f3b51e
Gagandeep singh <gaganrobin71@gmail.com>
2024-06-28 11:14:03 +0530

    datatypes

---
+++ 02_dataTypes.js
@@ -0,0 +1,30 @@
+"use strict";//treat all Js code as newer version
+
+// alert(3+3)// we are using in nodejs ,not browser
+
+console.log(3+3);//code readbilty should be high
+
+console.log("gagan")
+
+let name ="gagan"// string data type
+let age = 24  //number data type
+let islLoogedIn = false  //boolen data type
+let state = null // empty 
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+
+//boolen => true /false
+// null => standalone value
+
+// undefined => not assign value
+
+// symbol => unique
+
+//object 
+
+console.log(typeof undefined);// undefined
+console.log(typeof null);// object
+
