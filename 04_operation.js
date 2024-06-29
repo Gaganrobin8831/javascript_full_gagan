776771884afce1363a3b13688a57861bdf001a79
Gagandeep singh <gaganrobin71@gmail.com>
2024-06-29 12:14:51 +0530

    lear operation

---
+++ 04_operation.js
@@ -0,0 +1,40 @@
+let value = 3
+let negValue = -value
+// console.log(negValue); // output -3
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str = "hello"
+let str2 = " Gagan"
+let str3 = str + str2
+
+// console.log(str3); //hello Gagan
+
+// console.log("1"+2); //12
+// console.log(1+"2"); //12
+// console.log("1"+2 + 2); //122
+// console.log(1+2 + "2"); //32
+// console.log((3+ 4) * 5 % 3); //2
+
+// console.log(+true);//1
+// // console.log(true+);//erroe
+// console.log(true);//true
+// console.log(+"");//0
+
+let num1, num2, num3 
+ 
+num1 = num2 = num3 = 2+2 
+
+let gameCouter = 100
+
+// gameCouter++
+++gameCouter
+
+console.log(gameCouter);//101
+
