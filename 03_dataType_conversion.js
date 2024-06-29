5fbcab112faf4cc40696f3d09b23ff0e1fd43bac
Gagandeep singh <gaganrobin71@gmail.com>
2024-06-29 11:52:52 +0530

    coversion of data type

---
+++ 03_dataType_conversion.js
@@ -0,0 +1,37 @@
+// let score = "33abc" // output NaN
+// // let score = null //output 0
+// let score = undefined//output NaN
+// let score = true // output 1
+// let score = false // output 0
+// let score = "Gagan" // output NaN
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+// let  valueNumber = Number(score)
+
+// console.log(typeof valueNumber);
+// console.log(valueNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+
+// let islLoogedIn = 1 // true  ; // 0 => false
+//  let islLoogedIn = "" // false
+//  let islLoogedIn = "Gagan" // true
+
+// let boolenIsLoggedIn = Boolean(islLoogedIn)
+
+// console.log(boolenIsLoggedIn);
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+
+console.log(stringNumber);// 33
+console.log(typeof stringNumber);//string
+
