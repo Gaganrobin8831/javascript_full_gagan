685fceab5e4e4636c5c77abd1880348fb77ca004
Gagandeep singh <gaganrobin71@gmail.com>
2024-07-01 11:34:35 +0530

    primitives and non primitive

---
+++ datatypes_Summary.js
@@ -0,0 +1,41 @@
+// primitive
+
+// 7 types: String ,Number,Boolean,null,undefined,Symbol,BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const otutesideTemp =null
+let useEmail;
+
+const id =Symbol('123')
+const anotherId = Symbol('123')
+
+// console.log(id === anotherId);
+
+const bideNumber = 21435246576534234123345345n //for create bigInt just set n on last
+
+
+
+
+//Refernce Type(Non primitive)
+
+//Array ,Objects,Function
+
+const heroes =["shaktiman","nagraaj","daya"];
+
+let myObj={
+    name:"gagan",
+    age:25  
+}
+
+const MyFunction = function(){
+    console.log("Helo World");
+}
+
+
+console.log(typeof bideNumber);
+console.log(typeof otutesideTemp);
+console.log(typeof scoreValue);
+console.log(typeof MyFunction);
