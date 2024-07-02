22ede1817bf7098c7ad9f090cb974f0e2a62f0ac
Gagandeep singh <gaganrobin71@gmail.com>
2024-07-02 10:08:59 +0530

    learn memmory alloctaion with stact and heap

--- datatypes_Summary.js
+++ datatypes_Summary.js
@@ -2,19 +2,19 @@
 
 // 7 types: String ,Number,Boolean,null,undefined,Symbol,BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const otutesideTemp =null
-let useEmail;
+// const isLoggedIn = false
+// const otutesideTemp =null
+// let useEmail;
 
-const id =Symbol('123')
-const anotherId = Symbol('123')
+// const id =Symbol('123')
+// const anotherId = Symbol('123')
 
-// console.log(id === anotherId);
+// // console.log(id === anotherId);
 
-const bideNumber = 21435246576534234123345345n //for create bigInt just set n on last
+// const bideNumber = 21435246576534234123345345n //for create bigInt just set n on last
 
 
 
@@ -23,19 +23,45 @@ const bideNumber = 21435246576534234123345345n //for create bigInt just set n on
 
 //Array ,Objects,Function
 
-const heroes =["shaktiman","nagraaj","daya"];
+// const heroes =["shaktiman","nagraaj","daya"];
 
-let myObj={
-    name:"gagan",
-    age:25  
+// let myObj={
+//     name:"gagan",
+//     age:25  
+// }
+
+// const MyFunction = function(){
+//     console.log("Helo World");
+// }
+
+
+// console.log(typeof bideNumber);
+// console.log(typeof otutesideTemp);
+// console.log(typeof scoreValue);
+// console.log(typeof MyFunction);
+
+
+
+
+// **************************************************
+// Stack memory(primitive), Heap (Non-priitive)
+
+
+//stack (primitive) not effect original data its create copy of original
+let myName = "Gagan"
+let anotherName = myName
+
+anotherName ="Robin"
+// console.log(myName);
+// console.log(anotherName);
+
+let user ={
+    email:"gaganrobin71@gmail.com",
+    upi:"user@ybl"
 }
 
-const MyFunction = function(){
-    console.log("Helo World");
-}
+let userTwo = user
+userTwo.email = "afsgk124@gmail.com"
 
-
-console.log(typeof bideNumber);
-console.log(typeof otutesideTemp);
-console.log(typeof scoreValue);
-console.log(typeof MyFunction);
+console.log(user.email);
+console.log(userTwo.email);
