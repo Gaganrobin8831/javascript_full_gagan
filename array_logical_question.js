1b98ffe1c0200f0d3c9eea2d0c61735485f3e885
Gagandeep singh <gaganrobin71@gmail.com>
2024-07-12 11:48:18 +0530

    do some changes

--- array_logical_question.js
+++ array_logical_question.js
@@ -1,26 +1,29 @@
 const myArr = [0,1,2,1,1,1,2,3,4,3,5]
 let count=0;
-
+let temp = new Array;
 
 for (let index = 0; index < myArr.length; index++) {
     
     //console.log(myArr[index]);
-    let temp = myArr[index]
-//console.log(`this is temp value ${temp}`);
+   temp = myArr[index]
+// console.log(`this is temp value ${temp}`);
 
    for (let k = 0; k < myArr.length; k++) {
        if (temp == myArr[k]) {
          
             count++;
-       
+             
+          
+            
       }
-        
+      
      }
+
   
-
-
+     console.log(`the value is ${temp} as time ${count} array of ${index}`);
    
-        console.log(`the value is ${temp} as time ${count} array of ${j}`);
+   
+       
         
      
    
